0928a1cd1bd5a97a8a06c46f47812a77cfbb0573
Daniel Wirtz <daniel.wirtz@simtech.uni-stuttgart.de>
2014-11-06 18:56:20 +1300

    small changes, consistency commit

--- OpenCMISS.cmake
+++ OpenCMISS.cmake
@@ -1,8 +1,13 @@
+# OpenCMISS Dependencies main configuration file
+#
+# DO NOT EDIT.
+# Use the OpenCMISSLocalConfig.default.cmake file and copy it to OpenCMISSLocalConfig.cmake and do changes there.
+# If not copied manually, the defaults will be copied as-is.
 include(BuildMacros)
 
 if (NOT CMAKE_BUILD_TYPE AND NOT WIN32)
     SET(CMAKE_BUILD_TYPE RELEASE)
-    message(WARNING "No CMAKE_BUILD_TYPE has been defined. Using RELEASE.")
+    message(STATUS "No CMAKE_BUILD_TYPE has been defined. Using RELEASE.")
 endif()
 
 # The library path for all locally build dependencies
@@ -23,21 +28,34 @@ SET(OCM_DEPS_INSTALL_PREFIX ${CMAKE_CURRENT_SOURCE_DIR}/install${UNIXBUILDTYPEEX
 SET(OCM_DEPS BLAS LAPACK PLAPACK SCALAPACK METIS
     PARMETIS SUITESPARSE MUMPS SUPERLU SUPERLU_DIST
     SUNDIALS SCOTCH PTSCOTCH PASTIX HYPRE PETSC)
+
 # Dependencies using (any) MPI
+# Used to determine when MPI compilers etc should be passed down to packages
 SET(OCM_DEPS_WITHMPI MUMPS PARMETIS PASTIX PETSC
     PLAPACK SCALAPACK SCOTCH SUITESPARSE
     SUNDIALS SUPERLU_DIST)
+
 # Forward/downstream dependencies (for cmake build ordering and dependency checking)
+# Its organized this way as not all backward dependencies might be built by the cmake
+# system. here, the actual dependency list is filled "as we go" and actually build
+# packages locally, see ADD_DOWNSTREAM_DEPS in BuildMacros.cmake
+SET(HYPRE_FWD_DEPS PETSC)
 SET(LAPACK_FWD_DEPS SCALAPACK SUITESPARSE MUMPS
     SUPERLU SUPERLU_DIST METIS PARMETIS
     HYPRE SUNDIALS PASTIX PLAPACK PETSC)
 SET(METIS_FWD_DEPS MUMPS SUITESPARSE PASTIX)
+SET(MUMPS_FWD_DEPS PETSC)
 SET(PARMETIS_FWD_DEPS MUMPS SUITESPARSE SUPERLU_DIST)
-SET(SCALAPACK_FWD_DEPS MUMPS)
-SET(PTSCOTCH_FWD_DEPS PASTIX)
-SET(SCOTCH_FWD_DEPS PASTIX)
+SET(PASTIX_FWD_DEPS PETSC)
+SET(PTSCOTCH_FWD_DEPS PASTIX PETSC)
+SET(SCALAPACK_FWD_DEPS MUMPS PETSC)
+SET(SCOTCH_FWD_DEPS PASTIX PETSC)
+SET(SUNDIALS_FWD_DEPS PETSC)
+SET(SUPERLU_FWD_DEPS PETSC)
+SET(SUPERLU_DIST_FWD_DEPS PETSC)
 
 # Default: Build all dependencies
+# This is changeable in the OpenCMISSLocalConfig file
 FOREACH(OCM_DEP ${OCM_DEPS})
     SET(OCM_USE_${OCM_DEP} YES)
 ENDFOREACH()
